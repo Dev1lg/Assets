dd133ddf30c56cc11d62d05e2271ac1eda6e4779
Dev1lg <138635387+Dev1lg@users.noreply.github.com>
2023-07-25 10:43:38 +0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float RotationSpeed = 200f;
+
+    [Header("Shooting Area")]
+    public GameObject BulletPrefab;
+    public Transform BulletInitPos;
+    public float fireRate = 0.1f;
+    private float lastShot = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = moveDirection.magnitude;
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion toRotate = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, RotationSpeed * Time.deltaTime);
+        }
+
+        transform.Translate(transform.forward * magnitude * Speed * Time.deltaTime, Space.World);
+
+
+        if (Input.GetMouseButton(0))
+        {
+            //Fire
+            FireBullet();
+        }
+    }
+
+    void FireBullet()
+    {
+        if (Time.time > fireRate + lastShot)
+        {
+            Instantiate(BulletPrefab, BulletInitPos.position, BulletInitPos.rotation);
+            lastShot = Time.time;
+        }
+    }
+}
---
+++ Scriptableobjects/plane.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Plane props", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class planee : ScriptableObject
+{
+    public bool collideSometing;
+
+
+ }
+
+      
